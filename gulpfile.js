ed80224fa4cab6cd8932d1033d10338626377285
Pavlo Iermakov <ptopdruk@gmail.com>
2020-06-22 01:08:25 +0300

    step

---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+const gulp = require("gulp");
+const gulpSass = require("gulp-sass");
+const browserSync = require("browser-sync");
+const gulpAutoPrefixer = require("gulp-autoprefixer");
+const gulpConcat = require("gulp-concat");
+const gulpMinifycss = require("gulp-clean-css");
+const gulpImgmin = require("gulp-imagemin");
+const clean = require("gulp-clean");
+const uglify = require("gulp-uglify");
+
+gulp.task("browser-sync", function() {
+    browserSync.init({
+        server: {
+            baseDir: ".",
+            directory: true,
+        },
+        notify: false
+    })
+});
+gulp.task("html", function () {
+    return gulp
+        .src("src/index.html")
+        .pipe(gulp.dest("dist"))
+})
+gulp.task("scss", function (){
+    return gulp
+        .src("src/scss/**/*.scss")
+        .pipe(gulpSass())
+        .pipe(gulp.dest("dist/css"))
+        .pipe(browserSync.reload({stream: true}))
+});
+gulp.task("js",async function () {
+    return gulp
+         .src("src/js/*.js")
+         .pipe(gulpConcat("common.min.js"))
+         .pipe(uglify())
+         .pipe(gulp.dest("dist/js"))
+         .pipe(browserSync.reload({stream: true}))
+});
+gulp.task("watch", function() {
+    gulp.watch("src/scss/**/*.scss", gulp.parallel("scss"));
+    gulp.watch("*.html", gulp.parallel("code"));
+    gulp.watch(["dist/js/min.js", "src/js/**/*.js"], gulp.parallel("js"));
+});
+gulp.task("code", function () {
+    return gulp
+        .src("*.html")
+        .pipe(browserSync.reload({stream: true}))
+});
+gulp.task("clean", async function () {
+    return gulp
+        .src("dist/*", {read: false})
+        .pipe(clean())
+});
+gulp.task("modify",async function () {
+    return gulp
+        .src("src/scss/**/*.scss")
+        .pipe(gulpSass())
+        .pipe(gulpAutoPrefixer(["last 15 versions", "> 1%", "ie 8", "ie 7"], {cascade: true }))
+        .pipe(gulpConcat("style.min.css"))
+        .pipe(gulpMinifycss({compatibility: 'ie8'}))
+        .pipe(gulp.dest("dist/css"))
+});
+gulp.task("img", async function () {
+    gulp
+        .src("src/img/**/*")
+        .pipe(gulpImgmin())
+        .pipe(gulp.dest("dist/img"))
+});
+
+gulp.task("build", gulp.parallel("clean", "html", "modify", "js", "img"));
+gulp.task("dev", gulp.parallel("html", "modify", "js", "img", "browser-sync", "watch"));
