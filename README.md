5f79191f5eae5b18e258df3940052cf953d1238e
Pavlo Iermakov <ptopdruk@gmail.com>
2020-06-22 01:20:21 +0300

    Update 2020-06-21T22:20:15.461Z

--- README.md
+++
@@ -1,2 +0,0 @@
-# step-project-forkio
-
--- src/js/navbar.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    $('.header__burger').click(function() {
-        $('.header__burger, .header__menu, .header__span').toggleClass('active');
-    })
-});
